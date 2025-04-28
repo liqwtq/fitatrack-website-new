dd752cf4bd4a8d7ace13a480c7cce9d864bba51a
al071 <lisavasylieva83@gmail.com>
2025-04-28 17:41:05 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from starlette.staticfiles import StaticFiles
+import logging
+from datetime import datetime
+
+
+logger = logging.getLogger(__name__)
+
+app = FastAPI()
+
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory='templates')
+
+
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    this_year = datetime.now().year
+    return templates.TemplateResponse("index.html", {"request": request, "year":this_year})
